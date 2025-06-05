f27a2744ce41192057f6d52229cbe564a29a0fda
THChen2002 <terryhorse2002@gmail.com>
2025-06-06 00:21:29 +0800

    feature: 對戰成績畫面更新&後端

--- scripts/results.js
+++ scripts/results.js
@@ -1,5 +1,5 @@
 function fetchResultsData() {
-    fetchAndStoreData('results', function(response) {
+    fetchData('results', function(response) {
         // console.log('獲取賽程資料成功:', response);
 		
 		// 循環賽隊伍 & 分數
@@ -8,16 +8,32 @@ function fetchResultsData() {
             const matchNo = $this.data('match-no');
             const matchData = response.matches.find(match => match.matchNo === matchNo);
             if (matchData) {
-                $this.children('.team-container').empty().append(
-                    `<span class="team-box">${matchData.teams[0]} A1</span>
-                    <span>vs</span>
-                    <span class="team-box">${matchData.teams[1]} A2</span>`
-                );
-                $this.children('.score-container').empty().append(
-                    `<span class="score-box">${matchData.gameScore.split(':')[0]}</span>
-                    <span>:</span>
-                    <span class="score-box">${matchData.gameScore.split(':')[1]}</span>`
-                );
+                // 隊伍
+                const $teamLeft = $this.find('.team-container .team-left');
+                const $teamRight = $this.find('.team-container .team-right');
+                $teamLeft.text(matchData.teams[0]);
+                $teamRight.text(matchData.teams[1]);
+                
+                // 各局比分
+                matchData.setScores.forEach((setScore, index) => {
+                    const scores = setScore.split(':');
+                    const leftSetScore = parseInt(scores[0], 10);
+                    const rightSetScore = parseInt(scores[1], 10);
+                    let setClass = '';
+                    if (leftSetScore > rightSetScore) {
+                        setClass = 'left-win';
+                    } else if (rightSetScore > leftSetScore) {
+                        setClass = 'right-win';
+                    }
+                    const $setScoreElement = $this.find('.set-scores .set-score').eq(index);
+                    $setScoreElement.text(setScore).removeClass('unplayed').addClass(setClass);
+                });
+                
+                // 總比分
+                const leftScore = parseInt(matchData.gameScore.split(':')[0], 10);
+                const rightScore = parseInt(matchData.gameScore.split(':')[1], 10);
+                $this.find('.score-container .team-left').text(leftScore);
+                $this.find('.score-container .team-right').text(rightScore);
             }
         });
 
