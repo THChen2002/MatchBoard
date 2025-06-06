2b1ec5c8ada740250126601e657aac0b3921a491
THChen2002 <terryhorse2002@gmail.com>
2025-06-06 15:44:15 +0800

    update: 賽程表樣式

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -23,23 +23,36 @@ function fetchScheduleData() {
                 $this.text(matchData.gameScore);
             }
 		});
-
+		
+		// 複決賽隊伍及比分
 		$('.node-content').each(function() {
 			const $this = $(this);
 			const matchNo = $this.data('match-no');
 			const matchData = response.matches.find(match => match.matchNo === matchNo);
 
 			if (matchData) {
-				$this.children('.match-box').empty().append(
-					`<div class="team">
-						<span class="name">${matchData.teams[0]}</span>
-						<span class="score">${matchData.gameScore.split(':')[0]}</span>
-					</div>
-					<div class="team">
-						<span class="name">${matchData.teams[1]}</span>
-						<span class="score opponent">${matchData.gameScore.split(':')[1]}</span>
-					</div>`
-				);
+				// 更新隊伍名稱
+				$this.find('.team-blue .name').text(matchData.teams[0]);
+				$this.find('.team-red .name').text(matchData.teams[1]);
+				
+				// 更新分數
+				const scores = matchData.gameScore.split(':');
+				const blueScore = parseInt(scores[0]);
+				const redScore = parseInt(scores[1]);
+				
+				$this.find('.team-blue .score').text(blueScore);
+				$this.find('.team-red .score').text(redScore);
+				
+				// 判斷贏家並加上 winning-team class
+				const $blueTeam = $this.find('.team-blue');
+				const $redTeam = $this.find('.team-red');
+				
+				// 給贏的隊伍加上 winning-team class
+				if (blueScore > redScore) {
+					$blueTeam.addClass('winning-team');
+				} else if (redScore > blueScore) {
+					$redTeam.addClass('winning-team');
+				}
 			}
 		});
 
